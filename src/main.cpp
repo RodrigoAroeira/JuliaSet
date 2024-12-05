RodrigoAroeira/JuliaSet

commit 6b56fdda61847a461738e0d05d281c5ec7594ec1

perf: Avoiding unnecessary calculations
Current frame is skipped if mouse doesn't move from previous location

src/main.cpp
@@ -1,12 +1,13 @@
-#include "constants.hpp"
-#include "functions.hpp"
-
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
+#include <complex.h>
+
 #include <chrono>
 #include <cmath>
-#include <complex.h>
 #include <vector>
+
+#include "functions.hpp"
+#include "globals.hpp"
 
 int main() {
 
@@ -14,8 +15,8 @@ int main() {
 
   setupWindow(window);
 
-  std::vector<GLubyte> pixels(Constants::WIDTH * Constants::HEIGHT * 3);
-  GLubyte *pixelsData = pixels.data();
+  std::vector<GLubyte> pixels;
+  double mouseX_old = 0, mouseY_old = 0;
   double mouseX, mouseY;
   double a0, b0;
 
@@ -23,6 +24,7 @@ int main() {
   int CURRENT_FPS = 0;
   auto start = std::chrono::high_resolution_clock::now();
   while (!glfwWindowShouldClose(window)) {
+    pixels.resize(Globals::WIDTH * Globals::HEIGHT * 3);
     auto current = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = current - start;
 
@@ -38,6 +40,12 @@ int main() {
 
     glfwGetCursorPos(window, &mouseX, &mouseY);
 
+    if (mouseX == mouseX_old && mouseY == mouseY_old)
+      continue;
+
+    mouseX_old = mouseX;
+    mouseY_old = mouseY;
+
     mapMouseToComplex(mouseX, mouseY, a0, b0);
 
     updateTitle(window, CURRENT_FPS, a0, b0);
@@ -48,8 +56,8 @@ int main() {
 
     glRasterPos2i(-1, -1);
 
-    glDrawPixels(Constants::WIDTH, Constants::HEIGHT, GL_RGB, GL_UNSIGNED_BYTE,
-                 pixelsData);
+    glDrawPixels(Globals::WIDTH, Globals::HEIGHT, GL_RGB, GL_UNSIGNED_BYTE,
+                 pixels.data());
 
     glfwSwapBuffers(window);
 

src/functions.cpp
@@ -1,13 +1,15 @@
 #include "functions.hpp"
-#include "constants.hpp"
 
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
+
 #include <algorithm>
 #include <cmath>
 #include <complex>
 #include <iostream>
 #include <vector>
+
+#include "globals.hpp"
 
 int juliaFunction(std::complex<double> &z, const std::complex<double> &c,
                   int maxIter) {
@@ -37,10 +39,10 @@ double mapRange(double value, double in_min, double in_max, double out_min,
 }
 
 void mapMouseToComplex(double mouseX, double mouseY, double &a0, double &b0) {
-  a0 = mapRange(mouseX, 0, Constants::WIDTH, -Constants::X_LIM,
-                Constants::X_LIM);
-  b0 = mapRange(mouseY, 0, Constants::HEIGHT, Constants::Y_LIM,
-                -Constants::Y_LIM);
+  a0 = mapRange(mouseX, 0, Globals::WIDTH, -Globals::Constants::X_LIM,
+                Globals::Constants::X_LIM);
+  b0 = mapRange(mouseY, 0, Globals::HEIGHT, Globals::Constants::Y_LIM,
+                -Globals::Constants::Y_LIM);
 }
 
 void keypressCallback(GLFWwindow *window, int key, int scancode, int action,
@@ -74,10 +76,10 @@ void keypressCallback(GLFWwindow *window, int key, int scancode, int action,
       double a0, b0;
       iss >> a0 >> b0;
       double aMap, bMap;
-      aMap = mapRange(a0, -Constants::X_LIM, Constants::X_LIM, 0,
-                      Constants::WIDTH),
-      bMap = mapRange(b0, Constants::Y_LIM, -Constants::Y_LIM, 0,
-                      Constants::HEIGHT);
+      aMap = mapRange(a0, -Globals::Constants::X_LIM, Globals::Constants::X_LIM,
+                      0, Globals::WIDTH),
+      bMap = mapRange(b0, Globals::Constants::Y_LIM, -Globals::Constants::Y_LIM,
+                      0, Globals::HEIGHT);
 
       glfwSetCursorPos(window, aMap, bMap);
     }
@@ -87,30 +89,30 @@ void keypressCallback(GLFWwindow *window, int key, int scancode, int action,
 
 void renderJuliaSet(std::vector<GLubyte> &pixels, double a0, double b0) {
 
-  for (int y = 0; y < Constants::HEIGHT; ++y) {
-    for (int x = 0; x < Constants::WIDTH; ++x) {
-      int idx = (y * Constants::WIDTH + x) * 3;
+  for (int y = 0; y < Globals::HEIGHT; ++y) {
+    for (int x = 0; x < Globals::WIDTH; ++x) {
+      int idx = (y * Globals::WIDTH + x) * 3;
 
-      double a =
-          mapRange(x, 0, Constants::WIDTH, -Constants::X_LIM, Constants::X_LIM);
+      double a = mapRange(x, 0, Globals::WIDTH, -Globals::Constants::X_LIM,
+                          Globals::Constants::X_LIM);
 
       // for some reason, the y axis is inverted in the window
       // so we need to invert it in the mapping
-      double b = mapRange(y, 0, Constants::HEIGHT, Constants::Y_LIM,
-                          -Constants::Y_LIM);
+      double b = mapRange(y, 0, Globals::HEIGHT, Globals::Constants::Y_LIM,
+                          -Globals::Constants::Y_LIM);
 
       std::complex<double> c(a0, b0);
       std::complex<double> z(a, b);
 
-      int n = juliaFunction(z, c, Constants::MAX_ITERATIONS);
+      int n = juliaFunction(z, c, Globals::Constants::MAX_ITERATIONS);
 
-      if (n == Constants::MAX_ITERATIONS) {
+      if (n == Globals::Constants::MAX_ITERATIONS) {
         pixels[idx] = 0;
         pixels[idx + 1] = 0;
         pixels[idx + 2] = 0;
       } else {
-        int co =
-            static_cast<int>(mapRange(n, 0, Constants::MAX_ITERATIONS, 0, 255));
+        int co = static_cast<int>(
+            mapRange(n, 0, Globals::Constants::MAX_ITERATIONS, 0, 255));
 
         pixels[idx] = co;
         pixels[idx + 1] = co;
@@ -127,7 +129,7 @@ void setupWindow(GLFWwindow *&window) {
     exit(EXIT_FAILURE);
   }
 
-  window = glfwCreateWindow(Constants::WIDTH, Constants::HEIGHT, "Julia Set",
+  window = glfwCreateWindow(Globals::WIDTH, Globals::HEIGHT, "Julia Set",
                             nullptr, nullptr);
 
   if (!window) {

commit 0184dd00cc8a27477f84c5bf900fb9524fa3f423

fix: ensure GLFW events are being polled in every iteration
Previously `glfwPollEvents()` was being called only at the end of the
loop, causing event processing to be skipped completely in some
iterations.

Issue introduced in the `6b56fdda` (perf: Avoiding unnecessary
calculations)

src/main.cpp
@@ -24,8 +24,11 @@ int main() {
   int CURRENT_FPS = 0;
   auto start = std::chrono::high_resolution_clock::now();
   while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
     pixels.resize(Globals::WIDTH * Globals::HEIGHT * 3);
     auto current = std::chrono::high_resolution_clock::now();
+
     std::chrono::duration<double> elapsed = current - start;
 
     if (elapsed.count() >= 1) {
@@ -60,8 +63,6 @@ int main() {
                  pixels.data());
 
     glfwSwapBuffers(window);
-
-    glfwPollEvents();
   }
   glfwDestroyWindow(window);
   glfwTerminate();